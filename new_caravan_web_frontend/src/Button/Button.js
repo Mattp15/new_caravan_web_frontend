1502f623af4bb11d2a836f9b27d07eb40b08253c
mattp15 <matthewrphillips15@gmail.com>
2022-09-28 23:35:43 -0400

    button

--- new_caravan_web_frontend/src/Button/Button.js
+++ new_caravan_web_frontend/src/Button/Button.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 const Button = (props) => {
-  //   return <button type={props.type} value={props.value} onClick={props.onClick} />;
+  return <button type={props.type} value={props.value} onClick={props.onClick} />;
 };
