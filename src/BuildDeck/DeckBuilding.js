ece7578543e94abdcde73ecac3528d6dbd3d1112
mattp15 <matthewrphillips15@gmail.com>
2022-10-04 18:41:41 -0400

    prepull for merge

--- src/BuildDeck/DeckBuilding.js
+++ src/BuildDeck/DeckBuilding.js
@@ -2,22 +2,43 @@ import React, { useState, useEffect, useContext } from 'react';
 import { CardImage } from '../Card/CardImage';
 import { ContextContainer } from '../App';
 
+const comparingFunction = (a, b) => {
+  let comparison = 0;
+
+  return a._id < b._id ? (comparison -= 1) : (comparison += 1);
+};
+
 export const DeckBuilding = (props) => {
   const [building, setBuilding] = useState([]);
   const [allUsersCards, setAllUsersCards] = useState([]);
+  const [deckChunk, setDeckChunk] = useState([]);
   const { allCards, usersDeck, setUsersDeck } = useContext(ContextContainer);
   useEffect(() => {
     setAllUsersCards(allCards);
   }, []);
 
-  const handleClick = (target) => {
+  // useEffect(() => {
+  //   let goThrough = true;
+  //   if (goThrough) {
+  //     let sorted = [...building];
+  //     sorted.sort(comparingFunction);
+  //     setBuilding(sorted);
+  //     console.log(building);
+  //   }
+  //   return () => {
+  //     goThrough = false;
+  //     console.log(goThrough);
+  //   };
+  // });
+
+  const handleClickBottom = (target) => {
     if (!building.includes(target)) {
       setBuilding((prev) => [...prev, target]);
     }
-    console.log(building, 'bottombuilding');
+    // const sorted = building;
+    // console.log(sorted, 'presourt');
   };
   const handleClickTop = (target) => {
-    console.log(building, 'topRow');
     setBuilding((prev) => prev.filter((fil) => fil._id !== target._id));
   };
   const handleSave = () => {
@@ -25,16 +46,16 @@ export const DeckBuilding = (props) => {
       setUsersDeck(building);
       //redirect?
     }
-    console.log(usersDeck, 'usersDeck');
   };
+  const indexingDeckChunk = () => {};
   return (
     <div>
       <h1>toprow</h1>
       {/* bottom row {JSON.stringify(allCards)} */}
-      <ul className='top-row slick-slider'>
+      <ul className='top-row'>
         {building
           ? building.map((card, index) => {
-              const { _id, name, value, suit, set, ismodifier, face, back } = card;
+              const { _id, name, value, suit, set, ismodifier, face } = card;
               return (
                 <li key={_id + index} ismodifier={ismodifier}>
                   <CardImage onClick={handleClickTop} name={name} value={value} suit={suit} set={set} src={`${process.env.REACT_APP_CARAVAN_API}/images/${face}`} alt={`${name}of${suit}`} card={card} row='top' />
@@ -51,7 +72,7 @@ export const DeckBuilding = (props) => {
               const cardSide = building.includes(card) ? back + '.png' : face;
               return (
                 <li key={_id + index} ismodifier={ismodifier}>
-                  <CardImage onClick={handleClick} name={name} value={value} suit={suit} set={set} src={`${process.env.REACT_APP_CARAVAN_API}/images/${cardSide}`} alt={`${name}of${suit}`} card={card} row='bottom' />
+                  <CardImage onClick={handleClickBottom} name={name} value={value} suit={suit} set={set} src={`${process.env.REACT_APP_CARAVAN_API}/images/${cardSide}`} alt={`${name}of${suit}`} card={card} row='bottom' />
                 </li>
               );
             })
