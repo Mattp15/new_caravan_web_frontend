af0e1323b34207e42f93b6ad42dc711cc3ce5d34
mattp15 <matthewrphillips15@gmail.com>
2022-10-04 21:38:31 -0400

    started styling build page, needs some text

--- src/components/CardImg.jsx
+++ src/components/CardImg.jsx
@@ -1,10 +1,7 @@
-
+import './Card.css';
 const CardImg = (props) => {
-    const {src, alt, isFace} = props;
-    return(
-        <img crossOrigin='use-credentials' src={src} alt={alt} className={isFace?'face':'back'} />
-    );
+  const { src, alt, isFace } = props;
+  return <img crossOrigin='use-credentials' src={src} alt={alt} className={isFace ? 'face' : 'back'} />;
 };
-
 
 export default CardImg;
--- src/components/DoubleCardSlider.jsx
+++ src/components/DoubleCardSlider.jsx
@@ -1,34 +1,37 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import CardSlider from './CardSlider';
 import './DoubleCardSlider.css';
 
 const DoubleCardSlider = (props) => {
-const {ownedCards, cardsInDeck, onClick} = props;
-const [offset, setOffset] = useState(0);
+  const { ownedCards, cardsInDeck, onClick } = props;
+  const [offset, setOffset] = useState(0);
 
-const handleScrollLeft = () => {
+  const handleScrollLeft = () => {
     let tmp = offset - 7;
-    tmp = tmp>=0?tmp:0;
+    tmp = tmp >= 0 ? tmp : 0;
     setOffset(tmp);
-}
+  };
 
-const handleScrollRight = () => {
+  const handleScrollRight = () => {
     let tmp = offset + 7;
-    tmp = tmp<ownedCards.length-7?tmp:ownedCards.length-7;
+    tmp = tmp < ownedCards.length - 7 ? tmp : ownedCards.length - 7;
     setOffset(tmp);
-}
+  };
 
-    return (
-        <div className='double-slider-container'>
-            <div className='left-arrow' onClick={handleScrollLeft}>{'<==='}</div>
-            <div className='double-slider-body'>
-                <CardSlider offset={offset} isDeck={true} cardsInDeck={cardsInDeck} ownedCards={ownedCards} onClick={onClick} />
-                <CardSlider offset={offset} isDeck={false} cardsInDeck={cardsInDeck} ownedCards={ownedCards} onClick={onClick} />
-            </div>
-            <div className='right-arrow' onClick={handleScrollRight}>{'===>'}</div>
-        </div>
-    );
-}
-
+  return (
+    <div className='double-slider-container'>
+      <div className='left-arrow' onClick={handleScrollLeft}>
+        {'<==='}
+      </div>
+      <div className='double-slider-body'>
+        <CardSlider offset={offset} isDeck={true} cardsInDeck={cardsInDeck} ownedCards={ownedCards} onClick={onClick} />
+        <CardSlider offset={offset} isDeck={false} cardsInDeck={cardsInDeck} ownedCards={ownedCards} onClick={onClick} />
+      </div>
+      <div className='right-arrow' onClick={handleScrollRight}>
+        {'===>'}
+      </div>
+    </div>
+  );
+};
 
 export default DoubleCardSlider;
