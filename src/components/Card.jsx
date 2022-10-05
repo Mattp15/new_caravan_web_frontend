3a969afc046347921bbc71d55423d3d1b70bd75d
mattp15 <matthewrphillips15@gmail.com>
2022-10-04 20:40:48 -0400

    fixed bug for back of cards not rendering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,21 @@
-import CardImg  from "./CardImg";
+import CardImg from './CardImg';
 import './Card.css';
 
 const Card = (props) => {
-    const {card, idx, isFaceUp, onClick} = props;
-    return(
-        <>
-        {/* {console.log(card, idx, isFaceUp)} */}
-        <div className="card" onClick={()=>{onClick(idx)}}>
-            {isFaceUp?
-            <CardImg src={process.env.REACT_APP_CARAVAN_API + '/images/' + card.face} alt={card.name + ' of ' + card.suit} isFace={true} />
-            :<CardImg src={process.env.REACT_APP_CARAVAN_API + '/images/' + card.back} alt={'Card Back ' + card.set} isFace={false} />
-            }
-        </div>
-        </>
-    );
+  const { card, idx, isFaceUp, onClick } = props;
+  return (
+    <>
+      {/* {console.log(card, idx, isFaceUp)} */}
+      <div
+        className='card'
+        onClick={() => {
+          onClick(idx);
+        }}
+      >
+        {isFaceUp ? <CardImg src={process.env.REACT_APP_CARAVAN_API + '/images/' + card.face} alt={card.name + ' of ' + card.suit} isFace={true} /> : <CardImg src={process.env.REACT_APP_CARAVAN_API + '/images/' + card.back + '.png'} alt={'Card Back ' + card.set} isFace={false} />}
+      </div>
+    </>
+  );
 };
-
 
 export default Card;
